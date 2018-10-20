719cf52f6fdd31e6a343ff9a07c98cc0b0a200ec
LYQCOOL <38323955+LYQCOOL@users.noreply.github.com>
2018-10-20 20:27:50 +0800

    Update Pipe.py

--- cins/Pipe.py
+++ cins/Pipe.py
@@ -5,8 +5,7 @@ import pymongo
 
 class MongoDB(object):
     def __init__(self):
-        client = pymongo.MongoClient('182.150.37.55', 50070)
-        # client = pymongo.MongoClient('localhost',27017)
+        client = pymongo.MongoClient('localhost',27017)
 
         db = client['news']
         self.collection_zhengwen = db['content']
